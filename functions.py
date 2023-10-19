9a9dc0e1114f3ddcb59acb075d95c430cb868551
Joseph123231 <144241862+Joseph123231@users.noreply.github.com>
2023-10-19 13:28:47 +0000

    Complete

--- functions.py
+++ functions.py
@@ -1,17 +1,15 @@
+import math
 # TASK: Update both functions to reverse the letters in the name and provide the square root of the users age.
 
 # Returns a string that is the contents of the myName variable written backwards
 # ie: passing Atkinson as the parameter would return nosniktA
+import math
 def reverseName(myName):
-  # Write your code here
-  
-  result = myName
+  result = myName[::-1]
   return result
 
 # Returns a float value that is the contents of the myAge variable square rooted
 # ie: passing 16 as the parameter would return 4.0
 def rootAge(myAge):
-  # Wrie your code here
-  
-  result = myAge
+  result = math.sqrt(float(myAge)) 
   return result
